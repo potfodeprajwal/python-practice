b2e856f89d63cdda73fe1dfa6b32a5478ce2b3a8
PRAJWAL POTFODE <121285854+potfodeprajwal@users.noreply.github.com>
2024-07-21 23:38:09 +0530

    Update input.py

--- input.py
+++ input.py
@@ -1,32 +1,32 @@
 # # write a python program to add two input no 
-# a = int(input("enter no 1 :"))
-# b = int(input("enter no 2 :"))
-# print("number a is :",a)
-# print("number b is :",b)
-# print(a+b)
+a = int(input("enter no 1 :"))
+b = int(input("enter no 2 :"))
+print("number a is :",a)
+print("number b is :",b)
+print(a+b)
 
 
-# # write a python proggram to return remainder
-# a = int(input("enter first no :"))
-# b = int(input("enter second no :"))
-# print("remainder is :",a % b)
+# write a python proggram to return remainder
+a = int(input("enter first no :"))
+b = int(input("enter second no :"))
+print("remainder is :",a % b)
 
 
-# # write a python proggram to check type of variable
-# a = int(input("enter the value :"))
-# print(type(a)) 
+# write a python proggram to check type of variable
+a = int(input("enter the value :"))
+print(type(a)) 
 
 
-# #use comparison operator
-# a = int(input("enter first no :"))
-# b = int(input("enter second no :"))
-# print("a is greater than b :",a>b)
+#use comparison operator
+a = int(input("enter first no :"))
+b = int(input("enter second no :"))
+print("a is greater than b :",a>b)
 
 
-# #find avg
-# a = int(input("enter first no :"))
-# b = int(input("enter second no :"))
-# print("average is :",(a+b)/2)
+#find avg
+a = int(input("enter first no :"))
+b = int(input("enter second no :"))
+print("average is :",(a+b)/2)
 
 
 #find square of a no 
